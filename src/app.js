53550c62b1ce5f5f5bdc56ad32a79d8fc578b45c
AndresBouhut <andresbouhutdocumentos@gmail.com>
2025-03-07 20:00:49 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -4,16 +4,16 @@
   //write your code here
   window.onload = function() {
     
-    var first = "My ";
-    var noun = ["dog ", "grandma ", "mailman ", "bird ", "driver "];
-    var action = ["ate ", "peed ", "crushed ", "broke "];
-    var possetion = ["my homework ", "my phone ", "the car "];
-    var when = ["before the class", "when i was sleeping", "while i was exercising", "during my lunch", "while i was praying"];
+    let first = "My ";
+    let noun = ["dog ", "grandma ", "mailman ", "bird ", "driver "];
+    let action = ["ate ", "peed ", "crushed ", "broke "];
+    let possetion = ["my homework ", "my phone ", "the car "];
+    let when = ["before the class", "when i was sleeping", "while i was exercising", "during my lunch", "while i was praying"];
   
-    var rdm1 = Math.floor(Math.random() * noun.length);
-    var rdm2 = Math.floor(Math.random() * action.length);
-    var rdm3 = Math.floor(Math.random() * possetion.length);
-    var rdm4 = Math.floor(Math.random() * when.length);
+    let rdm1 = Math.floor(Math.random() * noun.length);
+    let rdm2 = Math.floor(Math.random() * action.length);
+    let rdm3 = Math.floor(Math.random() * possetion.length);
+    let rdm4 = Math.floor(Math.random() * when.length);
   
     document.querySelector("#excuse").innerHTML = first + noun[rdm1] + action[rdm2] + possetion[rdm3] + when[rdm4];
   
