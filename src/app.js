4c9c5dd673de7803d65fd1d13a084e0b58978e36
AndresBouhut <andresbouhutdocumentos@gmail.com>
2025-03-08 17:01:51 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -3,19 +3,24 @@
 
   //write your code here
   window.onload = function() {
+    let createExcuse = ()=>{
+      let first = "My ";
+      let who = ["dog ", "grandma ", "mailman ", "bird ", "driver "];
+      let action = ["ate ", "peed ", "crushed ", "broke "];
+      let possetion = ["my homework ", "my phone ", "the car "];
+      let when = ["before the class", "when i was sleeping", "while i was exercising", "during my lunch", "while i was praying"];
+  
+    return first + (who[Math.floor(Math.random()*(who.length))]) + (action[Math.floor(Math.random()*(action.length))]) + (possetion[Math.floor(Math.random()*(possetion.length))]) + (when[Math.floor(Math.random()*(when.length))]);
     
-    let first = "My ";
-    let noun = ["dog ", "grandma ", "mailman ", "bird ", "driver "];
-    let action = ["ate ", "peed ", "crushed ", "broke "];
-    let possetion = ["my homework ", "my phone ", "the car "];
-    let when = ["before the class", "when i was sleeping", "while i was exercising", "during my lunch", "while i was praying"];
+    
+    
+    
+   }
   
-    let rdm1 = Math.floor(Math.random() * noun.length);
-    let rdm2 = Math.floor(Math.random() * action.length);
-    let rdm3 = Math.floor(Math.random() * possetion.length);
-    let rdm4 = Math.floor(Math.random() * when.length);
-  
-    document.querySelector("#excuse").innerHTML = first + noun[rdm1] + action[rdm2] + possetion[rdm3] + when[rdm4];
+    
+    
+    document.getElementById("excuse").innerHTML = createExcuse()
+    
   
   }
   
